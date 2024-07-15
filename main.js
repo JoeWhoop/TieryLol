2b6bc7e3ef1525f29534cb8776484d0fccb4ed90
JoeWhoop <147258204+JoeWhoop@users.noreply.github.com>
2024-07-15 12:25:51 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+document.getElementById("goButton").addEventListener("click", function () {
+    var selectedMechanic = document.getElementById("mechanicDropdown").value;
+    var selectedStation = document.getElementById("stationDropdown").value;
+
+    selectedMechanic = selectedMechanic.toUpperCase().replace(" ", "_");
+    selectedStation = selectedStation.toUpperCase().replace(" ", "_");
+
+    var baseRedirectURL = "https://web.miniextensions.com/TAm18MNz3NlswuLvUliV?prefill_Mechanic=" + selectedMechanic;
+
+    var redirectionURLs = {
+        "D0": baseRedirectURL + "&prefill_Station=" + selectedStation,
+        "D1": baseRedirectURL + "&prefill_Station=" + selectedStation,
+        "D2": baseRedirectURL + "&prefill_Station=" + selectedStation,
+        "D3": baseRedirectURL + "&prefill_Station=" + selectedStation,
+        "D4": baseRedirectURL + "&prefill_Station=" + selectedStation
+        
+    };
+
+    if (redirectionURLs[selectedStation]) {
+        window.location.href = redirectionURLs[selectedStation];
+    } else {
+        alert("Please select both a mechanic and a station before clicking Go.");
+    }
+});
